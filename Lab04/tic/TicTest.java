6c25a875cdad24f8c149123fed0caf6181339c54
kris-evan N'guessant <bngue079@uottawa.ca>
2026-06-27 13:11:27 -0400

    Rest 2 ROUGE: Test placementde turnn X (echec)

--- Lab04/tic/TicTest.java
+++ Lab04/tic/TicTest.java
@@ -6,12 +6,17 @@ import org.junit.jupiter.api.Test;
 
 class TicTest {
 
-	@Test
+	@Test //testing ability to declare a board
 	void test() {
 		Tic board = new Tic(3, 3);
 		Tic board2 = new Tic(3,3);
 		assertEquals(board, board2);
-		//testing ability to declare a board
 	}
 	
+	@Test //testing ability to place a move on the board
+	public void testPlayMove() {
+		Tic game = new Tic(3, 3);
+		game.play(0, 0); 
+		assertEquals("X", game.board[0][0]); 
+	}
 }
