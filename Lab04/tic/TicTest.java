f26f525d6d2e6c5101d2331be557f38fc08db2ec
kris-evan N'guessant <bngue079@uottawa.ca>
2026-06-27 13:20:20 -0400

    Test3 ROUGE: Changement de tour (echec)

--- Lab04/tic/TicTest.java
+++ Lab04/tic/TicTest.java
@@ -6,17 +6,24 @@ import org.junit.jupiter.api.Test;
 
 class TicTest {
 
-	@Test //testing ability to declare a board
+	@Test // #1. testing ability to declare a board
 	void test() {
 		Tic board = new Tic(3, 3);
 		Tic board2 = new Tic(3,3);
 		assertEquals(board, board2);
 	}
 	
-	@Test //testing ability to place a move on the board
+	@Test // #2. testing ability to place a move on the board
 	public void testPlayMove() {
 		Tic game = new Tic(3, 3);
 		game.play(0, 0); 
 		assertEquals("X", game.board[0][0]); 
+	}
+
+	@Test // #3. testing ability to change turns after a move
+	public void testTurnChangesAfterMove() {
+		Tic game = new Tic(3, 3);
+		game.play(0, 0);
+		assertEquals("O", game.turn); 
 	}
 }
